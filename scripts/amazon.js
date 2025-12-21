815dd3bd06e9a03a6679208a7eebd5ae2d884995
Dizline <kazbekthekid@gmail.com>
2025-12-21 13:34:01 +0500

    A quantuty selector nowinteractive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach( (product) => {
             </div>
 
             <div class="product-quantity-container">
-                <select>
+                <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -53,6 +53,10 @@ products.forEach( (product) => {
             </button>
             </div>`
 })
+
+
+
+
 document.querySelector('.js-products-grid')
 .innerHTML = productsHTML
 
@@ -60,6 +64,11 @@ document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
+
+        const quantitySelector = document.querySelector(
+    `.js-quantity-selector-${productId}`
+);
+const quantity = Number(quantitySelector.value);
 
         let matchingItem;
 
@@ -70,12 +79,12 @@ document.querySelectorAll('.js-add-to-cart')
         })
 
         if (matchingItem) {
-            matchingItem.quantity+=1
+            matchingItem.quantity+=quantity
         }
         else {
             cart.push({
             productId: productId,
-            quantity: 1
+            quantity: quantity
         });
         }
 
@@ -85,5 +94,6 @@ document.querySelectorAll('.js-add-to-cart')
             cartQuantity += item.quantity;
         })
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+
     })
 })
