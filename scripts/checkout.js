f03b525cbdbe94838c22f23264f1ab59fb19baf6
Dizline <kazbekthekid@gmail.com>
2026-01-07 20:18:30 +0500

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,31 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProductsFetch, loadProducts } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
+    async function loadPage() {
+    try {
+        // throw 'error1';
+
+        await loadProductsFetch();
+
+        const value = await new Promise((resolve, reject) => {
+        // throw 'error2';
+        loadCart(() => {
+            // reject('error3');
+            resolve('value3');
+        });
+        });
+
+    } catch (error) {
+        console.log('Unexpected error. Please try again later.');
+    }
+
+    renderOrderSummary();
+    renderPaymentSummary();
+    }
+    loadPage();
+
+/*
+
 Promise.all([
     loadProductsFetch(),
     new Promise(resolve =>
@@ -14,6 +39,7 @@ Promise.all([
     renderPaymentSummary(); 
 });
 
+*/
 
 /*
 
